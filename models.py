1fa91b249303e9148fb64cf19b016430002ed058
sandhra <sandhrasonabijoy@gmail.com>
2025-01-15 09:16:05 +0900

    the models.py file is updated having the questions and choices class for the quiz

--- models.py
+++ models.py
@@ -1,2 +1,7 @@
 from sqlalchemy import Boolean,Column ,ForeignKey,Integer,String
+from database  import Base
 
+class Questions(Base):
+    __tablename__='questions'
+
+id=Column(Integer,primary_key=True,index=True)
